60b066dbbd009ae7c6550bded7219ab3f22cb330
thomassong <thomassong2012@gmail.com>
2023-01-17 10:37:16 +0800

    Fix potential deadlock in levelsController compaction #1795
    Signed-off-by: thomassong <thomassong2012@gmail.com>

--- levels.go
+++ levels.go
@@ -777,11 +777,15 @@ type compactDef struct {
 
 func (cd *compactDef) lockLevels() {
 	cd.thisLevel.RLock()
-	cd.nextLevel.RLock()
+	if cd.thisLevel.level != cd.nextLevel.level {
+		cd.nextLevel.RLock()
+	}
 }
 
 func (cd *compactDef) unlockLevels() {
-	cd.nextLevel.RUnlock()
+	if cd.thisLevel.level != cd.nextLevel.level {
+		cd.nextLevel.RUnlock()
+	}
 	cd.thisLevel.RUnlock()
 }
 
